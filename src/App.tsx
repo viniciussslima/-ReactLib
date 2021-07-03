dd25b511d97e007b9eac0758af5f68dbcbed2474
viniciussslima <vinicius.silva.017@ufrn.edu.br>
2021-07-02 21:12:34 -0300

    Implementing drawer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { Drawer } from "./lib";
 
 function App() {
-  return <div>viniciussslima lib</div>;
+  const [openDrawer, setOpenDrawer] = useState(false);
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          margin: "10px 500px",
+        }}
+      >
+        <button onClick={() => setOpenDrawer(true)}>open drawer</button>
+      </div>
+      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
+        <></>
+      </Drawer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/lib/Drawer/index.tsx
@@ -0,0 +1,81 @@
+import React, {
+  FC,
+  CSSProperties,
+  ReactNode,
+  useState,
+  useEffect,
+} from "react";
+import "./style.css";
+
+import defaultCloseIcon from "../assets/drawer/close.svg";
+
+interface DrawerProps {
+  open: boolean;
+  onClose: React.MouseEventHandler<HTMLImageElement>;
+  containerStyle?: CSSProperties;
+  contentStyle?: CSSProperties;
+  closeButton?: boolean;
+  closeIcon?: string;
+  closeIconStyle?: CSSProperties;
+  children: ReactNode;
+}
+
+const Drawer: FC<DrawerProps> = ({
+  open,
+  onClose,
+  containerStyle,
+  contentStyle,
+  closeButton = true,
+  closeIcon = defaultCloseIcon,
+  closeIconStyle,
+  children,
+}) => {
+  const [openControll, setOpenControll] = useState(false);
+
+  useEffect(() => {
+    if (open) {
+      setOpenControll(true);
+    } else {
+      setTimeout(() => setOpenControll(false), 800);
+    }
+  }, [open]);
+
+  const renderContent = () => {
+    return (
+      <>
+        {closeButton && (
+          <img
+            className="drawer-close-icon"
+            style={closeIconStyle}
+            src={closeIcon}
+            alt="X button to close the drawer"
+            onClick={onClose}
+          />
+        )}
+        <div className="drawer-content" style={contentStyle}>
+          {children}
+        </div>
+      </>
+    );
+  };
+  return (
+    <>
+      {openControll && open && (
+        <div className="drawer-background">
+          <div className="drawer-container drawer-open" style={containerStyle}>
+            {renderContent()}
+          </div>
+        </div>
+      )}
+      {openControll && !open && (
+        <div className="drawer-background">
+          <div className="drawer-container drawer-close" style={containerStyle}>
+            {renderContent()}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Drawer;
